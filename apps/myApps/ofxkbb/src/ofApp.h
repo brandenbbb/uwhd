ceecdd12fa06c7ce51323addd5b5ff257ea4f214
brandenbbb <branden.bb@gmail.com>
2015-04-13 00:23:55 -0400

    need to figure out how to get gamepad controller to control camera!

--- apps/myApps/ofxkbb/src/ofApp.h
+++ apps/myApps/ofxkbb/src/ofApp.h
@@ -46,6 +46,7 @@ public:
     
     void keyPressed(int key);
     void keyReleased(int key);
+    
     void mouseMoved(int x, int y );
     void mouseDragged(int x, int y, int button);
     void mousePressed(int x, int y, int button);
