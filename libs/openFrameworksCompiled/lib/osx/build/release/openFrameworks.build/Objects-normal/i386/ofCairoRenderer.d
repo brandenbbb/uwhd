51c9984f0ada54943858fa96299c13e534a5916a
brandenbbb <branden.bb@gmail.com>
2015-04-16 22:34:01 -0400

    stuff

--- libs/openFrameworksCompiled/lib/osx/build/release/openFrameworks.build/Objects-normal/i386/ofCairoRenderer.d
+++ libs/openFrameworksCompiled/lib/osx/build/release/openFrameworks.build/Objects-normal/i386/ofCairoRenderer.d
@@ -1,5 +1,5 @@
 dependencies: \
-  /Users/NFBHRS031/Desktop/uwhd/libs/openFrameworks/graphics/ofCairoRenderer.cpp \
+  /Users/mav/Desktop/git/uwhd/libs/openFrameworks/graphics/ofCairoRenderer.cpp \
   ../../../../libs/openFrameworks/graphics/ofCairoRenderer.h \
   ../../../../libs/cairo/include/cairo/cairo-features.h \
   ../../../../libs/cairo/include/cairo/cairo-pdf.h \
@@ -7,18 +7,18 @@ dependencies: \
   ../../../../libs/cairo/include/cairo/cairo-version.h \
   ../../../../libs/cairo/include/cairo/cairo-deprecated.h \
   ../../../../libs/cairo/include/cairo/cairo-svg.h \
-  /Users/NFBHRS031/Desktop/uwhd/libs/openFrameworksCompiled/project/osx/../../../openFrameworks/math/ofMatrix4x4.h \
+  /Users/mav/Desktop/git/uwhd/libs/openFrameworksCompiled/project/osx/../../../openFrameworks/math/ofMatrix4x4.h \
   ../../../../libs/openFrameworks/math/ofVec3f.h \
   ../../../../libs/openFrameworks/math/ofVec2f.h \
-  /Users/NFBHRS031/Desktop/uwhd/libs/openFrameworksCompiled/project/osx/../../../openFrameworks/utils/ofConstants.h \
+  /Users/mav/Desktop/git/uwhd/libs/openFrameworksCompiled/project/osx/../../../openFrameworks/utils/ofConstants.h \
   ../../../../libs/glew/include/GL/glew.h \
   ../../../../libs/tess2/include/tesselator.h \
   ../../../../libs/openFrameworks/math/ofVec4f.h \
   ../../../../libs/openFrameworks/math/ofQuaternion.h \
-  /Users/NFBHRS031/Desktop/uwhd/libs/openFrameworksCompiled/project/osx/../../../openFrameworks/types/ofBaseTypes.h \
+  /Users/mav/Desktop/git/uwhd/libs/openFrameworksCompiled/project/osx/../../../openFrameworks/types/ofBaseTypes.h \
   ../../../../libs/openFrameworks/types/ofPoint.h \
   ../../../../libs/openFrameworks/types/ofRectangle.h \
-  /Users/NFBHRS031/Desktop/uwhd/libs/openFrameworksCompiled/project/osx/../../../openFrameworks/utils/ofLog.h \
+  /Users/mav/Desktop/git/uwhd/libs/openFrameworksCompiled/project/osx/../../../openFrameworks/utils/ofLog.h \
   ../../../../libs/openFrameworks/utils/ofFileUtils.h \
   ../../../../libs/poco/include/Poco/File.h \
   ../../../../libs/poco/include/Poco/Foundation.h \
@@ -29,29 +29,29 @@ dependencies: \
   ../../../../libs/poco/include/Poco/Types.h \
   ../../../../libs/poco/include/Poco/Timestamp.h \
   ../../../../libs/poco/include/Poco/File_UNIX.h \
-  /Users/NFBHRS031/Desktop/uwhd/libs/openFrameworksCompiled/project/osx/../../../openFrameworks/types/ofTypes.h \
+  /Users/mav/Desktop/git/uwhd/libs/openFrameworksCompiled/project/osx/../../../openFrameworks/types/ofTypes.h \
   ../../../../libs/openFrameworks/types/ofColor.h \
-  /Users/NFBHRS031/Desktop/uwhd/libs/openFrameworksCompiled/project/osx/../../../openFrameworks/math/ofMath.h \
+  /Users/mav/Desktop/git/uwhd/libs/openFrameworksCompiled/project/osx/../../../openFrameworks/math/ofMath.h \
   ../../../../libs/poco/include/Poco/Mutex.h \
   ../../../../libs/poco/include/Poco/Exception.h \
   ../../../../libs/poco/include/Poco/ScopedLock.h \
   ../../../../libs/poco/include/Poco/Mutex_POSIX.h \
-  /Users/NFBHRS031/Desktop/uwhd/libs/openFrameworksCompiled/project/osx/../../../openFrameworks/3d/ofMesh.h \
-  /Users/NFBHRS031/Desktop/uwhd/libs/openFrameworksCompiled/project/osx/../../../openFrameworks/utils/ofUtils.h \
+  /Users/mav/Desktop/git/uwhd/libs/openFrameworksCompiled/project/osx/../../../openFrameworks/3d/ofMesh.h \
+  /Users/mav/Desktop/git/uwhd/libs/openFrameworksCompiled/project/osx/../../../openFrameworks/utils/ofUtils.h \
   ../../../../libs/poco/include/Poco/Path.h \
-  /Users/NFBHRS031/Desktop/uwhd/libs/openFrameworksCompiled/project/osx/../../../openFrameworks/gl/ofGLUtils.h \
-  /Users/NFBHRS031/Desktop/uwhd/libs/openFrameworksCompiled/project/osx/../../../openFrameworks/graphics/ofPixels.h \
+  /Users/mav/Desktop/git/uwhd/libs/openFrameworksCompiled/project/osx/../../../openFrameworks/gl/ofGLUtils.h \
+  /Users/mav/Desktop/git/uwhd/libs/openFrameworksCompiled/project/osx/../../../openFrameworks/graphics/ofPixels.h \
   ../../../../libs/openFrameworks/graphics/ofPath.h \
   ../../../../libs/openFrameworks/graphics/ofPolyline.h \
-  /Users/NFBHRS031/Desktop/uwhd/libs/openFrameworksCompiled/project/osx/../../../openFrameworks/gl/ofVboMesh.h \
+  /Users/mav/Desktop/git/uwhd/libs/openFrameworksCompiled/project/osx/../../../openFrameworks/gl/ofVboMesh.h \
   ../../../../libs/openFrameworks/gl/ofVbo.h \
   ../../../../libs/openFrameworks/graphics/ofTessellator.h \
   ../../../../libs/openFrameworks/graphics/ofGraphics.h \
-  /Users/NFBHRS031/Desktop/uwhd/libs/openFrameworks/app/ofAppRunner.h \
+  /Users/mav/Desktop/git/uwhd/libs/openFrameworks/app/ofAppRunner.h \
   ../../../../libs/openFrameworks/graphics/ofImage.h \
-  /Users/NFBHRS031/Desktop/uwhd/libs/openFrameworksCompiled/project/osx/../../../openFrameworks/gl/ofTexture.h \
-  /Users/NFBHRS031/Desktop/uwhd/libs/openFrameworksCompiled/project/osx/../../../openFrameworks/3d/of3dPrimitives.h \
+  /Users/mav/Desktop/git/uwhd/libs/openFrameworksCompiled/project/osx/../../../openFrameworks/gl/ofTexture.h \
+  /Users/mav/Desktop/git/uwhd/libs/openFrameworksCompiled/project/osx/../../../openFrameworks/3d/of3dPrimitives.h \
   ../../../../libs/openFrameworks/3d/ofNode.h \
-  /Users/NFBHRS031/Desktop/uwhd/libs/openFrameworksCompiled/project/osx/../../../openFrameworks/math/ofVectorMath.h \
+  /Users/mav/Desktop/git/uwhd/libs/openFrameworksCompiled/project/osx/../../../openFrameworks/math/ofVectorMath.h \
   ../../../../libs/openFrameworks/math/ofMatrix3x3.h \
   ../../../../libs/openFrameworks/3d/of3dUtils.h
--- libs/openFrameworksCompiled/lib/osx/build/release/openFrameworks.build/Objects-normal/i386/ofQtUtils.d
+++ libs/openFrameworksCompiled/lib/osx/build/release/openFrameworks.build/Objects-normal/i386/ofQtUtils.d
@@ -1,6 +1,6 @@
 dependencies: \
-  /Users/NFBHRS031/Desktop/uwhd/libs/openFrameworks/video/ofQtUtils.cpp \
+  /Users/mav/Desktop/git/uwhd/libs/openFrameworks/video/ofQtUtils.cpp \
   ../../../../libs/openFrameworks/video/ofQtUtils.h \
-  /Users/NFBHRS031/Desktop/uwhd/libs/openFrameworksCompiled/project/osx/../../../openFrameworks/utils/ofConstants.h \
+  /Users/mav/Desktop/git/uwhd/libs/openFrameworksCompiled/project/osx/../../../openFrameworks/utils/ofConstants.h \
   ../../../../libs/glew/include/GL/glew.h \
   ../../../../libs/tess2/include/tesselator.h
