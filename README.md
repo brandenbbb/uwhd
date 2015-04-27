87231d251745796194b2638cdb14c61971ef344a
brandenbbb <branden.bb@gmail.com>
2015-04-26 23:25:01 -0400

    Revert 13dff2e..e8494c5
    This rolls back to commit 13dff2e6071a14f848964a44e058369bdf47c983.

--- README.md
+++ README.md
@@ -4,30 +4,32 @@ APRIL 20:
     kinect(1) = host kinect
     kinect(0) = guest kinect
     removed diagnostics mode
+    h key on keyboard hides host mode GUI
+
 
 APRIL 19:
     xbox 360 USB gamepad controls now working if USE_GAMEPAD enabled. FPS style controls
     also...be sure to compile as release as the framerate performance drastically increases
 
-APRIL 14:
-    added switch in ofApp.h - simply comment / uncomment to enable which mode you want to use:
 
+SWITCH MODES
+    in ofApp.h, comment out any of the following to enable various modes / or for debugging
+    
+    USE_KINECT
+        if both this and USE_TWO_KINECTS are commented out, it will disable all Kinect stuff for debugging purposes
 
-USE_KINECT
-    if both this and USE_TWO_KINECTS are commented out, it will disable all Kinect stuff for debugging purposes
+    USE_TWO_KINECTS
+        use two kinect point clouds
 
-USE_TWO_KINECTS
-    use two kinect point clouds
+    USE_GAMEPAD
+        use Xbox360 gamepad
+        *REQUIRES XBOX 360 controller driver to be installed which is in the root folder
 
-USE_GAMEPAD
-    use Xbox360 gamepad
-    *REQUIRES XBOX 360 controller driver to be installed which is in the root folder
+    USE_PHOTOBOOTH
+        enable photobooth mode with 2D backgrounds and peasy camera instead of FPS camera
 
-USE_PHOTOBOOTH
-    enable photobooth mode with 2D backgrounds and peasy camera instead of FPS camera
-
-USE_HOSTMODE
-    enable live host mode
+    USE_HOSTMODE
+        enable live host mode
 
 
 KEYBOARD CONTROLS!
@@ -70,13 +72,13 @@ IMAGE WRITER:
 
     F1 = full screen
 
-    F4 = point cloud mode / live mode
+    F4 = point cloud mode / go back to live mode (if in review last frame mode)
         
     F5 = save PNG of entire OF window
 
     F6 = review last saved file (saved into the data/bin folder)
 
-    F4 = go back to live point cloud (during review mode)
+    h = hide GUI in host mode
 
 
 POINT CLOUD ADJUSTMENTS:
@@ -96,10 +98,6 @@ POINT CLOUD ADJUSTMENTS:
 
 KINECT MOTOR ADJUSTMENTS:
 
-    0 = zero the angle of the kinect
+    n = increase angle of Kinect
 
-    o = return to last set angle
-
-    key arrow up = increase angle of Kinect
-
-    key arrow down = decrease angle of Kinect
+    m = decrease angle of Kinect
--- apps/myApps/ofxkbb/src/ofApp.cpp
+++ apps/myApps/ofxkbb/src/ofApp.cpp
@@ -49,6 +49,40 @@ void ofApp::setup() {
     // 3d model load assets
     towers.loadModel("images/3d/towersandtrees.dae");
     sphere.loadModel("images/3d/skysphere.dae");
+    
+    // 3d object movement GUI setup
+    
+    moveThings = new ofxUISuperCanvas("3D OBJECTS");
+    moveThings ->addLabel("HOST POINT CLOUD");
+    moveThings ->addSpacer();
+    moveThings ->addSlider("hostTranX", -5000, 5000, hostTranX);
+    moveThings ->addSlider("hostTranY", -5000, 5000, hostTranY);
+    moveThings ->addSlider("hostTranZ", -5000, 5000, hostTranZ);
+    moveThings ->addSpacer();
+    moveThings ->addSlider("hostRoX", -180, 180, hostRoX);
+    moveThings ->addSlider("hostRoY", -180, 180, hostRoY);
+    moveThings ->addSlider("hostRoZ", -180, 180, hostRoZ);
+    moveThings ->addSpacer();
+    moveThings ->addLabel("GUEST POINT CLOUD");
+    moveThings ->addSpacer();
+    moveThings ->addSlider("guestTranX", -5000, 5000, guestTranX);
+    moveThings ->addSlider("guestTranY", -5000, 5000, guestTranY);
+    moveThings ->addSlider("guestTranZ", -5000, 5000, guestTranZ);
+    moveThings ->addSpacer();
+    moveThings ->addSlider("guestRoX", -180, 180, guestRoX);
+    moveThings ->addSlider("guestRoY", -180, 180, guestRoY);
+    moveThings ->addSlider("guestRoZ", -180, 180, guestRoZ);
+    moveThings ->addLabel("TOWERS");
+    moveThings ->addSpacer();
+    moveThings ->addSlider("towersScale", 0, 20, towersScale);
+    moveThings ->addButton("reset_towersScale", false);
+    moveThings ->addSlider("towersTranX", -5000, 5000, towersTranX);
+    moveThings ->addSlider("towersTranY", -5000, 5000, towersTranY);
+    moveThings ->addSlider("towersTranZ", -5000, 5000, towersTranZ);
+    moveThings ->addSpacer();
+    moveThings ->autoSizeToFitWidgets();
+    ofAddListener(moveThings ->newGUIEvent,this,&ofApp::guiEvent);
+    moveThings ->loadSettings("moveThingsSettings.xml");
 #endif
 }
 
@@ -99,8 +133,8 @@ void ofApp::draw() {
         #endif
     
         // 3D towers!
-        towers.setScale(.5, -.5, .5);
-        towers.setPosition(0, -100, -100);
+        towers.setScale(towersScale*.5, towersScale*-.5, towersScale*.5);
+        towers.setPosition(towersTranX, towersTranY, towersTranZ);
         towers.drawFaces();
         camera.end();
 #endif
@@ -163,12 +197,18 @@ void ofApp::drawHostPointCloud() {
 	ofPushMatrix();
 	// the projected points are 'upside down' and 'backwards' 
 	ofScale(1, -1, -1);
+    
 #ifdef USE_HOSTMODE
-	ofTranslate(0, 0, 0); // center the points a bit
+    ofRotateX(hostRoX);
+    ofRotateY(hostRoY);
+    ofRotateZ(hostRoZ);
+    ofTranslate(hostTranX, hostTranY, hostTranZ);
 #endif
+    
 #ifdef USE_PHOTOBOOTH
     ofTranslate(0, 0, -1000); // center the points a bit
 #endif
+    
 	ofEnableDepthTest();
 	mesh.drawVertices();
 	ofDisableDepthTest();
@@ -195,8 +235,13 @@ void ofApp::drawGuestPointCloud() {
     glPointSize(pointSize);
     ofPushMatrix();
     // the projected points are 'upside down' and 'backwards'
-    ofScale(1, -1, 1);
-    ofTranslate(0, 0, 0); // center the points a bit;
+    ofScale(1, -1, -1);
+    
+    ofTranslate(guestTranX, guestTranY, guestTranZ);
+    ofRotateX(guestRoX);
+    ofRotateX(guestRoY);
+    ofRotateZ(guestRoZ);
+    
     ofEnableDepthTest();
     mesh2.drawVertices();
     ofDisableDepthTest();
@@ -217,39 +262,130 @@ void ofApp::exit() {
     pboothGUI->saveSettings("photoBoothSettings.xml");
     delete pboothGUI;
 #endif
+    
+    moveThings->saveSettings("moveThingsSettings.xml");
+    delete moveThings;
 }
 
 
+// photo booth writer GUI
 //--------------------------------------------------------------
 
 void ofApp::photoBoothGUI(){
     pboothGUI = new ofxUISuperCanvas("EMAIL");
-    
     pboothGUI->addSpacer();
     pboothGUI->setWidgetFontSize(OFX_UI_FONT_MEDIUM);
     email = pboothGUI->addTextInput("EMAIL", "");
     emailFile = ofToString(email);
-    
     ofAddListener(pboothGUI->newGUIEvent,this,&ofApp::guiEvent);
 }
 
-
+// move things GUI
 //--------------------------------------------------------------
 void ofApp::guiEvent(ofxUIEventArgs &e)
 {
     string name = e.getName();
     int kind = e.getKind();
-    cout << "got event from: " << name << endl;
     
-    if(name == "EMAIL")
-    {
+    if(name == "EMAIL"){
         ofxUITextInput *email = (ofxUITextInput *) e.widget;
-        if(email->getInputTriggerType() == OFX_UI_TEXTINPUT_ON_ENTER)
-        {
-            cout << "ON ENTER: ";
-        }
         emailFile = email->getTextString();
-        cout << emailFile << endl;
+    }
+    
+    // HOST REPOSITIONING
+    if(name == "hostTranX")
+    {
+        ofxUISlider *hostTranX_Slider = (ofxUISlider *) e.widget;
+        hostTranX = hostTranX_Slider->getScaledValue();
+    }
+    if(name == "hostTranY")
+    {
+        ofxUISlider *hostTranY_Slider = (ofxUISlider *) e.widget;
+        hostTranY = hostTranY_Slider->getScaledValue();
+    }
+    if(name == "hostTranZ")
+    {
+        ofxUISlider *hostTranZ_Slider = (ofxUISlider *) e.widget;
+        hostTranZ = hostTranZ_Slider->getScaledValue();
+    }
+    if(name == "hostRoX")
+    {
+        ofxUISlider *hostRoX_Slider = (ofxUISlider *) e.widget;
+        hostRoX = hostRoX_Slider->getScaledValue();
+    }
+    if(name == "hostRoY")
+    {
+        ofxUISlider *hostRoY_Slider = (ofxUISlider *) e.widget;
+        hostRoY = hostRoY_Slider->getScaledValue();
+    }
+    if(name == "hostRoZ")
+    {
+        ofxUISlider *hostRoZ_Slider = (ofxUISlider *) e.widget;
+        hostRoZ = hostRoZ_Slider->getScaledValue();
+    }
+    
+    // GUEST REPOSITIONING
+    if(name == "guestTranX")
+    {
+        ofxUISlider *guestTranX_Slider = (ofxUISlider *) e.widget;
+        guestTranX = guestTranX_Slider->getScaledValue();
+    }
+    if(name == "guestTranY")
+    {
+        ofxUISlider *guestTranY_Slider = (ofxUISlider *) e.widget;
+        guestTranY = guestTranY_Slider->getScaledValue();
+    }
+    if(name == "guestTranZ")
+    {
+        ofxUISlider *guestTranZ_Slider = (ofxUISlider *) e.widget;
+        guestTranZ = guestTranZ_Slider->getScaledValue();
+    }
+    if(name == "guestRoX")
+    {
+        ofxUISlider *guestRoX_Slider = (ofxUISlider *) e.widget;
+        guestRoX = guestRoX_Slider->getScaledValue();
+    }
+    if(name == "guestRoY")
+    {
+        ofxUISlider *guestRoY_Slider = (ofxUISlider *) e.widget;
+        guestRoY = guestRoY_Slider->getScaledValue();
+    }
+    if(name == "guestRoZ")
+    {
+        ofxUISlider *guestRoZ_Slider = (ofxUISlider *) e.widget;
+        guestRoZ = guestRoZ_Slider->getScaledValue();
+    }
+    
+    // TOWERS REPOSITIONING
+    if(name == "towersScale"){
+        ofxUISlider *towersScale_Slider = (ofxUISlider *) e.widget;
+        towersScale = towersScale_Slider->getScaledValue();
+    }
+    if(name == "reset_towersScale"){
+        ofxUIButton *button = (ofxUIButton *) e.getButton();
+        if (button->getValue() == true){
+            towersScale = 1;
+        }
+    }
+    if(kind == OFX_UI_WIDGET_BUTTON)
+    {
+        ofxUIButton *button = (ofxUIButton *) e.widget;
+        cout << name << "\t value: " << button->getValue() << endl;
+    }
+    if(name == "towersTranX")
+    {
+        ofxUISlider *towersTranX_Slider = (ofxUISlider *) e.widget;
+        towersTranX = towersTranX_Slider->getScaledValue();
+    }
+    if(name == "towersTranY")
+    {
+        ofxUISlider *towersTranY_Slider = (ofxUISlider *) e.widget;
+        towersTranY = towersTranY_Slider->getScaledValue();
+    }
+    if(name == "towersTranZ")
+    {
+        ofxUISlider *towersTranZ_Slider = (ofxUISlider *) e.widget;
+        towersTranZ = towersTranZ_Slider->getScaledValue();
     }
 }
 
@@ -274,6 +410,11 @@ void ofApp::keyPressed (int key) {
         // image review keystroke
         case OF_KEY_F6:
             bReviewLastShot = true;
+            break;
+        
+        // hide GUI
+        case 'h':
+            moveThings->toggleVisible();
             break;
             
 		case '.':
@@ -313,13 +454,13 @@ void ofApp::keyPressed (int key) {
 			kinect.close();
 			break;
 			
-		case OF_KEY_UP:
+		case 'n':
 			angle++;
 			if(angle>30) angle=30;
 			kinect.setCameraTiltAngle(angle);
 			break;
 			
-		case OF_KEY_DOWN:
+		case 'm':
 			angle--;
 			if(angle<-30) angle=-30;
 			kinect.setCameraTiltAngle(angle);
